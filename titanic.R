fd2e1f9faac5d5d38fe3fca84b725b732e535625
pradeep4130 <pradeeprai4130@gmail.com>
2018-10-23 02:57:51 -0500

    5

--- titanic.R
+++ titanic.R
@@ -36,12 +36,25 @@ for (x in 1:length(titanic_o$age)) {
 
 View(titanic_o)
 
+# 3: Lifeboat
 
+#titanic_o$boat <- replace(titanic_o$boat, which(is.na(titanic_o$boa)), 'NA')
+#titanic_o %>% filter(titanic_o,is.na(titanic_o$boat))
+#titanic_o$boat[is.na(titanic_o$boat)] <- "NONE"
+for (x in 1:length(titanic_o$boat)) {
+  if(titanic_o$boat[x] == "") {titanic_o$boat[x] <- "NA"}
+}
 
+View(titanic_o)
 
+# Column is filled with NA
 
+# 4: Cabin
 
+colnames(titanic_o)
+titanic_o <- titanic_o %>% mutate('has_cabin_number' = ifelse(titanic_o$cabin == "",0,1))
 
+View(titanic_o)
 
 
 
